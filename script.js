8bdc60e645cf6ee7d2093b04191b588b6be82e82
Peyton Bechard <pbechard@my.yorku.ca>
2022-03-31 16:51:42 +0800

    Adds functionality for clear button and grid resizing

--- script.js
+++ script.js
@@ -6,30 +6,54 @@ createGrid(gridDimension);
 
 // GRID CREATION
 function createGrid(dimension) {
-    for (let i = 0; i < gridDimension; i++) {
-        for (let i = 0; i < gridDimension; i++) {
+    currentGridBoxes = grid.querySelectorAll('div');
+    currentGridBoxes.forEach( (div) => div.remove() );
+
+    for (let i = 0; i < dimension; i++) {
+        for (let i = 0; i < dimension; i++) {
             const newDiv = document.createElement('div');
             grid.append(newDiv);
             newDiv.classList.add('grid-box');
-            newDiv.style.width = `${100/gridDimension}%`;
-            newDiv.style.height = `${100/gridDimension}%`;
+            newDiv.style.width = `${100/dimension}%`;
+            newDiv.style.height = `${100/dimension}%`;
         }
     }
-}
+};
 
 
 // DIMENSION INPUT
 const validDimension = /^[\d]+$/;
 const dimensionInput = document.getElementById('grid-dimension');
+const setButton = document.getElementById('set-grid-button');
+setButton.setAttribute('disabled', 'true');
+
 dimensionInput.addEventListener("keyup", (e) => {
     let input = dimensionInput.value;
     if (!input) {
         dimensionInput.style.borderColor = "rgb(88, 88, 92)";
+        setButton.setAttribute('disabled', 'true');
     } else if (input.match(validDimension)) {
-        console.log('match');
         dimensionInput.style.borderColor = "rgb(0, 93, 23)";
+        setButton.removeAttribute('disabled');
     } else {
-        console.log('no match');
         dimensionInput.style.borderColor = "rgb(126, 3, 3)";
+        setButton.setAttribute('disabled', 'true');
     }
-})
+});
+
+
+// OPTIONS MENU
+const clearCurrentButton = document.getElementById('clear-button');
+clearCurrentButton.addEventListener('click', (e) => {
+    let allGridBoxes = grid.querySelectorAll('div');
+    allGridBoxes.forEach( (div) => { div.style.color = "lightgray" })
+});
+
+
+setButton.addEventListener('click', () => {
+    gridDimension = dimensionInput.value;
+    intDimension = parseInt(gridDimension);
+    createGrid(intDimension);
+});
+
+
